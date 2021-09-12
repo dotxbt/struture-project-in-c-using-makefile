2dcd766ddcee20211ec421e2f6f49cc6841524c5
DOT XBT <dotxbt@gmail.com>
2021-09-12 17:35:49 +0700

    change Point

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include "../inc/main.h"
 
 int main() {
+
+    // POINT
     Point p1 = createPoint(3,4);
     Point p2 = createPoint(2,10);
     Point p = addPoint(p1, p2);
     printPoint(p);
+    setPoint(&p, 40, 100);
+    printPoint(p);
+
     return 0;
 }
